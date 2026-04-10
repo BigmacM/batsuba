30916c3a5a91d732a5bbfc6d1cb3ec39a2002ed4
BigmacM <maksut890@gmail.com>
2026-04-10 07:13:24 -0400

    Update LINE link to https://lin.ee/q1xDDFNt

--- CONFIG.ts
+++ CONFIG.ts
@@ -88,7 +88,7 @@ export const SITE_CONFIG = {
     facebook: "https://web.facebook.com/people/%E0%B8%9A%E0%B8%B8%E0%B8%A9%E0%B8%9A%E0%B8%B2-%E0%B9%84%E0%B8%A7%E0%B8%99%E0%B9%8C%E0%B8%84%E0%B8%B2%E0%B9%80%E0%B8%9F%E0%B9%88-Butsaba-WineCafe/61573732255850/",
     instagram: "https://www.instagram.com/butsaba_wine_cafe",
     instagramHandle: "@butsaba_wine_cafe",
-    line: "https://line.me/R/ti/p/@butsaba",   // update with actual LINE ID
+    line: "https://lin.ee/q1xDDFNt",   // update with actual LINE ID
     tiktok: "",   // add if available
   },
 
